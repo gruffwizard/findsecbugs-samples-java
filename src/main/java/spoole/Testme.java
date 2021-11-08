bcd76f7ad7900c56f25826c7578fdda297b14c1b
Steve Poole <spoole167@googlemail.com>
2021-11-08 07:20:57 +0000

    Update Testme.java

--- src/main/java/spoole/Testme.java
+++ src/main/java/spoole/Testme.java
@@ -9,7 +9,7 @@ public class Testme {
   
   public int check(Data[] input) {
     
-    for (int i = 0; i < input.length(); i++) {
+    for (int i = 0; i < input.length; i++) {
     String data = input[i].data;
     for (int j = 0; i < 100; j++) {
         if (data != null) {
